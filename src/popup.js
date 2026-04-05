7f9c240cec3abca53cc0ff75c3bcfbc42a4ac3df
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2026-04-06 02:51:51 +0900

    feat: 단축키 기능 추가

--- src/popup.js
+++ src/popup.js
@@ -90,4 +90,15 @@ document.addEventListener("DOMContentLoaded", () => {
       chrome.runtime.openOptionsPage();
     });
   }
+
+  // 1~9 숫자 키보드 입력 시 해당하는 순서의 버튼(새 탭 열기) 클릭 이벤트 실행
+  document.addEventListener("keydown", (e) => {
+    if (e.key >= "1" && e.key <= "9") {
+      const index = parseInt(e.key, 10) - 1; // 1~9를 인덱스 0~8로 변환
+      const items = document.querySelectorAll(".grid-item");
+      if (items[index]) {
+        items[index].click();
+      }
+    }
+  });
 });
